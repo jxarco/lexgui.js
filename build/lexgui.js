2d8089a3cf98f7bcc7fc203649987466dbc6c875
evalls <evallsg4@gmail.com>
2023-10-11 12:08:09 +0200

    compare threshold range function fixed and lower timeout on minimize area setted

--- build/lexgui.js
+++ build/lexgui.js
@@ -739,7 +739,7 @@
                         setTimeout(() => {
                             area2.hide();
                             this._moveSplit(20);
-                        }, 400);
+                        }, 200);
                     }
                     // Fade in from down
                     else if(this.min.classList.contains("fa-angle-up")) {
@@ -6746,7 +6746,7 @@
 	LX.UTILS = {
         getTime() { return new Date().getTime() },
         compareThreshold( v, p, n, t ) { return Math.abs(v - p) >= t || Math.abs(v - n) >= t },
-        compareThresholdRange( v0, v1, t0, t1 ) { return v0 > t0 && v0 <= t1 || v1 > t0 && v1 <= t1 },
+        compareThresholdRange( v0, v1, t0, t1 ) { return v0 >= t0 && v0 <= t1 || v1 >= t0 && v1 <= t1 || v0 <= t0 && v1 >= t1},
         clamp (num, min, max) { return Math.min(Math.max(num, min), max) }
     };
     
