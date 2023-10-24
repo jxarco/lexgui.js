1a2939cb231886e1d50755c057f70f4aaf6f56c4
evalls <evallsg4@gmail.com>
2023-10-24 15:57:57 +0200

    smooth property added to Curve widget to join points with curved lines

--- build/lexgui.js
+++ build/lexgui.js
@@ -5868,7 +5868,7 @@
             element.allow_add_values = options.allow_add_values ?? true;
             element.draggable_x = options.draggable_x ?? true;
             element.draggable_y = options.draggable_y ?? true;
-
+            element.smooth = (options.smooth && typeof(options.smooth) == 'number' ? options.smooth : 0.3) || false;
             element.options = options;
             element.style.minWidth = "50px";
             element.style.minHeight = "20px";
@@ -5955,7 +5955,7 @@
                 if(o.value) element.value = o.value;
                 if(o.xrange) element.xrange = o.xrange;
                 if(o.yrange) element.yrange = o.yrange;
-
+                if(o.smooth) element.smooth = o.smooth;
                 var rect = canvas.parentElement.getBoundingClientRect();
                 if(canvas.parentElement.parentElement) rect = canvas.parentElement.parentElement.getBoundingClientRect();
                 if(rect && canvas.width != rect.width && rect.width && rect.width < 1000)
@@ -5977,16 +5977,27 @@
                 //draw line
                 var pos = convert([element.xrange[0],element.defaulty]);
                 ctx.moveTo( pos[0], pos[1] );
+                let values = [pos[0], pos[1]];
 
                 for(var i in element.value) {
                     var value = element.value[i];
                     pos = convert(value);
-                    ctx.lineTo( pos[0], pos[1] );
+                    values.push(pos[0]);
+                    values.push(pos[1]);
+                    if(!element.smooth)
+                        ctx.lineTo( pos[0], pos[1] );
                 }
 
                 pos = convert([element.xrange[1],element.defaulty]);
-                ctx.lineTo( pos[0], pos[1] );
-                ctx.stroke();
+                values.push(pos[0]);
+                values.push(pos[1]);
+                if(!element.smooth) {
+                    ctx.lineTo( pos[0], pos[1] );
+                    ctx.stroke();
+                } else {
+
+                    LX.UTILS.drawSpline(ctx, values, element.smooth);
+                }
 
                 //draw points
                 for(var i = 0; i < element.value.length; i += 1) {
@@ -6962,7 +6973,66 @@
         getTime() { return new Date().getTime() },
         compareThreshold( v, p, n, t ) { return Math.abs(v - p) >= t || Math.abs(v - n) >= t },
         compareThresholdRange( v0, v1, t0, t1 ) { return v0 >= t0 && v0 <= t1 || v1 >= t0 && v1 <= t1 || v0 <= t0 && v1 >= t1},
-        clamp (num, min, max) { return Math.min(Math.max(num, min), max) }
+        clamp (num, min, max) { return Math.min(Math.max(num, min), max) },
+        getControlPoints(x0,y0,x1,y1,x2,y2,t) {
+            //  x0,y0,x1,y1 are the coordinates of the end (knot) pts of this segment
+            //  x2,y2 is the next knot -- not connected here but needed to calculate p2
+            //  p1 is the control point calculated here, from x1 back toward x0.
+            //  p2 is the next control point, calculated here and returned to become the 
+            //  next segment's p1.
+            //  t is the 'tension' which controls how far the control points spread.
+            
+            //  Scaling factors: distances from this knot to the previous and following knots.
+            var d01=Math.sqrt(Math.pow(x1-x0,2)+Math.pow(y1-y0,2));
+            var d12=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        
+            var fa=t*d01/(d01+d12);
+            var fb=t-fa;
+        
+            var p1x=x1+fa*(x0-x2);
+            var p1y=y1+fa*(y0-y2);
+        
+            var p2x=x1-fb*(x0-x2);
+            var p2y=y1-fb*(y0-y2);  
+            
+            return [p1x,p1y,p2x,p2y]
+        },
+   
+        drawSpline(ctx,pts,t){
+        
+            ctx.save();
+            var cp=[];   // array of control points, as x0,y0,x1,y1,...
+            var n=pts.length;
+
+            // Draw an open curve, not connected at the ends
+            for(var i=0;i<n-4;i+=2){
+                cp=cp.concat(LX.UTILS.getControlPoints(pts[i],pts[i+1],pts[i+2],pts[i+3],pts[i+4],pts[i+5],t));
+            }    
+            for(var i=2;i<pts.length-5;i+=2){
+                ctx.beginPath();
+                ctx.moveTo(pts[i],pts[i+1]);
+                ctx.bezierCurveTo(cp[2*i-2],cp[2*i-1],cp[2*i],cp[2*i+1],pts[i+2],pts[i+3]);
+                ctx.stroke();
+                ctx.closePath();
+                
+            }
+            //  For open curves the first and last arcs are simple quadratics.
+            ctx.beginPath();
+            ctx.moveTo(pts[0],pts[1]);
+            ctx.quadraticCurveTo(cp[0],cp[1],pts[2],pts[3]);
+            ctx.stroke();
+            ctx.closePath();
+            
+        
+            ctx.beginPath();
+            ctx.moveTo(pts[n-2],pts[n-1]);
+            ctx.quadraticCurveTo(cp[2*n-10],cp[2*n-9],pts[n-4],pts[n-3]);
+            ctx.stroke();
+            ctx.closePath();
+            
+        ctx.restore();
+       
+        }
+
     };
-    
 })( typeof(window) != "undefined" ? window : (typeof(self) != "undefined" ? self : global ) );
