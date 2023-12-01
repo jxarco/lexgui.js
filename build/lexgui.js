f835bb8ee9707737648e13856b4bc6e7b1a7a5b7
jxarco <alexroco.30@gmail.com>
2023-12-01 16:16:36 +0100

    Update version

--- build/lexgui.js
+++ build/lexgui.js
@@ -12,7 +12,7 @@ console.warn( 'Script "build/lexgui.js" is depracated and will be removed soon. 
     */
 
     var LX = global.LX = {
-        version: "0.1.5",
+        version: "0.1.6",
         ready: false,
         components: [], // specific pre-build components
         signals: {} // events and triggers
--- build/lexgui.module.js
+++ build/lexgui.module.js
@@ -8,7 +8,7 @@
 */
 
 var LX = {
-    version: "0.1.5",
+    version: "0.1.6",
     ready: false,
     components: [], // specific pre-build components
     signals: {} // events and triggers
