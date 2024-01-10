d7d6c02355f7dc9ed2cbedd5762649685a49453a
jxarco <alexroco.30@gmail.com>
2024-01-10 02:39:46 +0100

    0.1.19

--- build/lexgui.module.js
+++ build/lexgui.module.js
@@ -8,7 +8,7 @@
 */
 
 var LX = {
-    version: "0.1.18",
+    version: "0.1.19",
     ready: false,
     components: [], // specific pre-build components
     signals: {} // events and triggers
---
+++ changelog.md
@@ -0,0 +1,5 @@
+# lexgui.js changelog
+
+## 0.1.19
+
+Add file explorer to Code Editor component. Minor fixes.
