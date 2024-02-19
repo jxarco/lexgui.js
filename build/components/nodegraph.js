fd6d74fc7eea42f4dc245b14e809715fd4cc52f0
jxarco <alexroco.30@gmail.com>
2024-02-19 15:13:54 +0100

    Start nodegraph refactor

--- build/components/nodegraph.js
+++ build/components/nodegraph.js
@@ -39,22 +39,23 @@ function doAsync( fn, ms ) {
 }
 
 /**
- * @class GraphCanvas
+ * @class GraphRenderer
  */
 
-class GraphCanvas {
+class GraphRenderer {
 
     static __instances  = [];
 
-    static BACK_IMAGE_SRC = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAAAXNSR0IArs4c6QAAAplJREFUeF7tm8FqwzAQRG2DDynEvaT//5fNKXFRCqZQkjLSyEzxC/Qmb5eZN9pYasfL5WMd+MQoMBZDxnEc5nl+2tS6rkP5mabpZeNlzf1+f6wrNZ99jrau6HC73f7UpazbDDmfFwsl1+vncDq9WWqVIkerhyGN6LiBwRAM0RRwE5hej4RofPxa7TYYQzBEU8BNYHo9EqLxsc+WVX7Lsrw3tvb9eDqB6f2RkEYM3QZjCIZoCrgJTK9HQjQ+9hnq5WSWw8U6Z9yJIyF1PmxPYUiYgBiCIZoCbmKOVu8xQ4rkr65wNUtY3aIAQ71FvQ5HRRiCIZoCh5whvBhqkPxc7QaGLaveiy7XDRiCIZoC7i0hvR4J0fjgtDedaHd/25s6d+p1UeliCF9768zo8UcdzJB6L/ja26hdFwHZshpdcQvorseWFWYwhmCIpoB7S0ivR0I0PnhTTyfa3R936o0JcT/OltWoaJeEcHRS7wqG1GvHm3qjdv9CQBLS6LJbQHc9hnqYwRiCIZoC7i0hvR4J0fjY5+ik/Bbu1OuccSeOhNT5sD2FIWECYgiGaAq4iTlaPWaIxts+37I47a13xZ1gElLvRZfDTwzBEE0B95aQXo87dY2P7qvZsholdicOQzBEU8BNYHo9EqLxwYthOtHu/kgICdEUcBOYXo+EaHwwQ9KJdvfH/6mTEE0BN4Hp9ZghGh/MkHSi3f2REBKiKeAmML0eCdH4YIakE+3uj4SQEE0BN4Hp9bhT1/jovpotq1Fid+IwBEM0BdwEptcjIRofvIekE+3uj4SQEE0BN4Hp9UiIxgczJJ1od38khIRoCrgJTK9HQjQ+mCHpRLv7IyEkRFPATWB6PRKi8cEMSSfa3R8JISGaAm4C0+t9AfQNJzgm7/oIAAAAAElFTkSuQmCC";
+    static MIN_SCALE    = 0.25;
+    static MAX_SCALE    = 1.5;
 
     // Node Drawing
     static NODE_TITLE_HEIGHT    = 24;
     static NODE_ROW_HEIGHT      = 16;
 
-    static NODE_SHAPE_RADIUS    = 4;
-    static NODE_TITLE_RADIUS    = [GraphCanvas.NODE_SHAPE_RADIUS, GraphCanvas.NODE_SHAPE_RADIUS, 0, 0];
-    static NODE_BODY_RADIUS     = [GraphCanvas.NODE_SHAPE_RADIUS, GraphCanvas.NODE_SHAPE_RADIUS, GraphCanvas.NODE_SHAPE_RADIUS, GraphCanvas.NODE_SHAPE_RADIUS];
+    static NODE_SHAPE_RADIUS    = 12;
+    static NODE_TITLE_RADIUS    = [ GraphRenderer.NODE_SHAPE_RADIUS, GraphRenderer.NODE_SHAPE_RADIUS, 0, 0 ];
+    static NODE_BODY_RADIUS     = [ GraphRenderer.NODE_SHAPE_RADIUS, GraphRenderer.NODE_SHAPE_RADIUS, GraphRenderer.NODE_SHAPE_RADIUS, GraphRenderer.NODE_SHAPE_RADIUS ];
 
     static DEFAULT_NODE_TITLE_COLOR     = "#4a59b0";
     static DEFAULT_NODE_BODY_COLOR      = "#111";
@@ -66,68 +67,54 @@ class GraphCanvas {
 
     constructor( area, options = {} ) {
 
-        GraphCanvas.__instances.push( this );
+        GraphRenderer.__instances.push( this );
 
         this.base_area = area;
-        this.area = new LX.Area( { className: "lexGraph" } );
+        this.area = new LX.Area( { className: "lexgraph" } );
 
-        area.root.classList.add('grapharea');
+        area.root.classList.add( 'grapharea' );
 
         this.root = this.area.root;
         area.attach( this.root );
 
         // Bind resize
+
         area.onresize = ( bb ) => {
-            this.dom.width = bb.width;
-            this.dom.height = bb.height;
-            this._backDirty = true;
-            this._frontDirty = true;
+            
         };
 
-        this.root.addEventListener( 'keydown', this._processKey.bind(this), true);
-        this.root.addEventListener( 'mousedown', this._processMouse.bind(this) );
-        this.root.addEventListener( 'mouseup', this._processMouse.bind(this) );
-        this.root.addEventListener( 'mousemove', this._processMouse.bind(this) );
-        this.root.addEventListener( 'click', this._processMouse.bind(this) );
-        this.root.addEventListener( 'contextmenu', this._processMouse.bind(this) );
-        this.root.addEventListener( 'focus', this._processFocus.bind(this, true) );
-        this.root.addEventListener( 'focusout', this._processFocus.bind(this, false) );
+        this.root.addEventListener( 'keydown', this._processKey.bind( this ), true );
+        this.root.addEventListener( 'mousedown', this._processMouse.bind( this ) );
+        this.root.addEventListener( 'mouseup', this._processMouse.bind( this ) );
+        this.root.addEventListener( 'mousemove', this._processMouse.bind( this ) );
+        this.root.addEventListener( 'mousewheel', this._processMouse.bind(this) );
+        this.root.addEventListener( 'click', this._processMouse.bind( this ) );
+        this.root.addEventListener( 'contextmenu', this._processMouse.bind( this ) );
+        this.root.addEventListener( 'focus', this._processFocus.bind( this, true) );
+        this.root.addEventListener( 'focusout', this._processFocus.bind( this, false ) );
 
-        // State
+        this._lastMousePosition = new LX.vec2( 0, 0 );
 
-        this.drawAllFrames      = false;
-        this.isFocused          = false;
-        this._backDirty         = true;
-        this._frontDirty        = true;
+        // Back pattern
+        
+        const f = 30.0;
+        this._patternPosition = new LX.vec2( 0, 0 );
+        this._patternSize = new LX.vec2( f );
+        this._circlePatternSize = f * 0.03;
+        this._circlePatternColor = '#71717a';
 
-        // Canvas
+        this._generatePattern();
 
-        this.dom = document.createElement('canvas');
-        this.dom.width = area.size[0];
-        this.dom.height = area.size[1];
-        this.dom.tabIndex = -1;
-        this.area.attach( this.dom );
+        // Renderer state
 
-        this.frames         = 0;
-        this.fps            = 0;
-        this._lastDrawTime  = 0;
-        this._drawTime      = 0;
+        this._scale = 1.0;
 
-        this.font = new FontFace("Ubuntu", "url(../data/Ubuntu-Bold.ttf)");
-        this.font.load().then(
-            ( font ) => {
-                document.fonts.add( font );
-                requestAnimationFrame( this.frame.bind(this) );
-            },
-            (err) => {
-                console.error(err);
-            },
-        );
+        // requestAnimationFrame( this.frame.bind(this) );
     }
 
     static getInstances()
     {
-        return GraphCanvas.__instances;
+        return GraphRenderer.__instances;
     }
 
     _processFocus( active ) {
@@ -135,13 +122,18 @@ class GraphCanvas {
         this.isFocused = active;
     }
 
-    _processKey(e) {
+    _processKey( e ) {
 
         var key = e.key ?? e.detail.key;
         console.log( key );
     }
 
-    _processMouse(e) {
+    _processMouse( e ) {
+
+        const rect = this.root.getBoundingClientRect();
+        
+        this._mousePosition = new LX.vec2( e.clientX - rect.x , e.clientY - rect.y );
+        // this._deltaMousePosition = this._mousePosition.sub( this._lastMousePosition );
 
         if( e.type == 'mousedown' )
         {
@@ -150,14 +142,14 @@ class GraphCanvas {
         
         else if( e.type == 'mouseup' )
         {
-            if( (LX.getTime() - this.lastMouseDown) < 300 ) {
-                this._processClick(e);
+            if( (LX.getTime() - this.lastMouseDown) < 120 ) {
+                this._processClick( e );
             }
         }
 
         else if( e.type == 'mousemove' )
         {
-            
+            this._processMouseMove( e );
         }
 
         else if ( e.type == 'click' ) // trip
@@ -171,15 +163,51 @@ class GraphCanvas {
             }
         }
 
-        else if ( e.type == 'contextmenu' ) {
-            e.preventDefault()
-            this._processContextMenu( e );
+        else if ( e.type == 'mousewheel' ) {
+            e.preventDefault();
+            this._processWheel( e );
         }
+
+        else if ( e.type == 'contextmenu' ) {
+
+            e.preventDefault();
+            
+            if( (LX.getTime() - this.lastMouseDown) < 120 ) {
+                this._processContextMenu( e );
+            }
+        }
+
+        this._lastMousePosition = this._mousePosition;
     }
 
     _processClick( e ) {
 
         
+    }
+
+    _processMouseMove( e ) {
+
+        const rightPressed = ( e.which == 3 );
+        
+        if( rightPressed )
+        {
+            this._updatePattern();
+
+            e.stopPropagation();
+            e.stopImmediatePropagation();
+        }
+    }
+
+    _processWheel( e ) {
+
+        const delta = e.deltaY;
+
+        if( delta > 0.0 ) this._scale *= 0.95;
+        else this._scale *= 1.05;
+
+        this._scale = LX.UTILS.clamp( this._scale, GraphRenderer.MIN_SCALE, GraphRenderer.MAX_SCALE );
+
+        this._updatePattern();
     }
 
     _processContextMenu( e ) {
@@ -188,12 +216,6 @@ class GraphCanvas {
             m.add( "option 1", () => { } );
             m.add( "option 2", () => { } );
         });
-    }
-
-    _forceDraw() {
-
-        this._backDirty = true;
-        this._frontDirty = true;
     }
 
     /**
@@ -220,7 +242,8 @@ class GraphCanvas {
 
     frame() {
 
-        // this.update();
+        this.update();
+
         this.draw();
 
         requestAnimationFrame( this.frame.bind(this) );
@@ -241,74 +264,7 @@ class GraphCanvas {
 
     draw() {
 
-        if (!this.dom || !this.dom.width || !this.dom.height)
-            return;
-
-        // Count Fps
-        var now = LX.getTime();
-        this._drawTime = (now - this._lastDrawTime) * 0.001;
-        this._lastDrawTime = now;
-
-        // if (this.graph) {
-        //     this.ds.computeVisibleArea(this.viewport);
-        // }
-
-        const forceDraw = this.drawAllFrames || (this._backDirty || this._frontDirty);
-
-        if ( forceDraw )
-        {
-            if( this._backDirty ) 
-                this._drawBack();
-            if ( this._frontDirty )
-                this._drawFront();
-        }
-
-        this.fps = this._drawTime ? (1.0 / this._drawTime) : 0;
-        this.frames += 1;
-    }
-
-    _drawBack() {
-
-        console.log( "_drawBack" );
-
-        var ctx = this.dom.getContext("2d");
-
-        if ( !GraphCanvas.BACK_IMAGE_SRC )
-            return;
-
-        ctx.imageSmoothingEnabled = false;
-
-        if ( !this._backImage ) {
-            this._backImage = new Image();
-            this._backImage.src = GraphCanvas.BACK_IMAGE_SRC;
-            this._backImage.onload = this._forceDraw.bind(this);
-        }
-
-        if ( !this._pattern && this._backImage.width > 0) {
-            this._pattern = ctx.createPattern(this._backImage, "repeat");
-        }
-
-        // Draw background
-
-        if (this._pattern) {
-            ctx.fillStyle = this._pattern;
-            ctx.fillRect(0, 0, this.dom.width, this.dom.height);
-            ctx.fillStyle = "transparent";
-        }
-        
-        ctx.globalAlpha = 1.0;
-        ctx.imageSmoothingEnabled = true;
-
-        // Draw node connections
-
-        this._drawConnections();
-
-        this._backDirty = false;
-    }
-
-    _drawFront() {
-
-        console.log( "_drawFront" );
+        console.log("Draw");
 
         let nodes = this._getVisibleNodes();
 
@@ -316,16 +272,71 @@ class GraphCanvas {
         {
             this._drawNode( node );
         }
-
-        this._frontDirty = false;
     }
 
-    _resetCanvasShadows( ctx ) {
+    _generatePattern() {
 
-        ctx.shadowOffsetX = 0;
-        ctx.shadowOffsetY = 0;
-        ctx.shadowBlur = 0;
-        ctx.shadowColor = "rgba(0,0,0,0)";
+        // Generate pattern
+        {
+            var pattern = document.createElementNS( 'http://www.w3.org/2000/svg', 'pattern' );
+            pattern.setAttribute( 'id', 'pattern-0' );
+            pattern.setAttribute( 'x', this._patternPosition.x );
+            pattern.setAttribute( 'y', this._patternPosition.y );
+            pattern.setAttribute( 'width', this._patternSize.x )
+            pattern.setAttribute( 'height', this._patternSize.y );
+            pattern.setAttribute( 'patternUnits', 'userSpaceOnUse' );
+
+            var circle = document.createElementNS( 'http://www.w3.org/2000/svg', 'circle' );
+            circle.setAttribute( 'cx', this._circlePatternSize );
+            circle.setAttribute( 'cy', this._circlePatternSize );
+            circle.setAttribute( 'r', this._circlePatternSize );
+            circle.setAttribute( 'fill', this._circlePatternColor );
+
+            pattern.appendChild( circle );
+        }
+        
+        var svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
+        svg.classList.add( "background-svg" );
+        svg.style.width = "100%";
+        svg.style.height = "100%";
+
+        svg.appendChild( pattern );
+
+        var rect = document.createElementNS( 'http://www.w3.org/2000/svg', 'rect' );
+        rect.setAttribute( 'x', '0' );
+        rect.setAttribute( 'y', '0' );
+        rect.setAttribute( 'width', '100%' );
+        rect.setAttribute( 'height', '100%' );
+        rect.setAttribute( 'fill', 'url(#pattern-0)' );
+
+        svg.appendChild( rect );
+
+        this._background = svg;
+
+        this.root.appendChild( this._background );
+    }
+
+    _updatePattern() {
+
+        if( !this._background )
+            return;
+
+        const patternSize = this._patternSize.mul( this._scale );
+        const circlePatternSize = this._circlePatternSize * this._scale;
+        
+        const deltaPosition = this._patternPosition.sub( this._mousePosition );
+        this._patternPosition = this._patternPosition.sub( deltaPosition );
+
+        let pattern = this._background.querySelector( 'pattern' );
+        pattern.setAttribute( 'x', this._patternPosition.x );
+        pattern.setAttribute( 'y', this._patternPosition.y );
+        pattern.setAttribute( 'width', patternSize.x )
+        pattern.setAttribute( 'height', patternSize.y );
+
+        var circle = this._background.querySelector( 'circle' );
+        circle.setAttribute( 'cx', circlePatternSize );
+        circle.setAttribute( 'cy', circlePatternSize );
+        circle.setAttribute( 'r', circlePatternSize );
     }
 
     _computeNodeSize( node ) {
@@ -336,7 +347,7 @@ class GraphCanvas {
         let sX = 32 + textMetrics.width * 1.475;
 
         const rows = Math.max(1,  Math.max(node.inputs.length, node.outputs.length));
-        let sY = rows * GraphCanvas.NODE_ROW_HEIGHT + GraphCanvas.NODE_TITLE_HEIGHT;
+        let sY = rows * GraphRenderer.NODE_ROW_HEIGHT + GraphRenderer.NODE_TITLE_HEIGHT;
 
         return [sX, sY];
     }
@@ -376,51 +387,7 @@ class GraphCanvas {
 
     _drawNode( node ) {
 
-        console.log( node.name );
-
-        // Process some attributes
-        node.size = node.size ?? this._computeNodeSize( node );
-        node.color = node.color ?? GraphCanvas.DEFAULT_NODE_BODY_COLOR;
-        node.titleColor = node.titleColor ?? GraphCanvas.DEFAULT_NODE_TITLE_COLOR;
-
-        let [pX, pY] = node.position;
-        let [sX, sY] = node.size;
-
-        const ctx = this.dom.getContext("2d");
-        const offsetY = GraphCanvas.NODE_TITLE_HEIGHT;
-
-        // Body
-
-        ctx.shadowBlur = 8;
-        ctx.shadowColor = "#000";
-
-        ctx.beginPath();
-        ctx.fillStyle = node.color;
-        ctx.roundRect( pX, pY, sX, sY, GraphCanvas.NODE_BODY_RADIUS );
-        ctx.fill();
-
-        this._resetCanvasShadows( ctx );
-
-        // Draw border
-        ctx.beginPath();
-        ctx.strokeStyle = "#555";
-        ctx.roundRect( pX, pY, sX, sY, GraphCanvas.NODE_BODY_RADIUS );
-        ctx.stroke();
-
-        // Title
-
-        ctx.beginPath();
-        var titleGrd = ctx.createLinearGradient(pX, pY, pX + sX, pY + offsetY);
-        titleGrd.addColorStop(0, node.color);
-        titleGrd.addColorStop(1, node.titleColor);
-
-        ctx.fillStyle = titleGrd;
-        ctx.roundRect( pX + 1, pY, sX - 2, offsetY, GraphCanvas.NODE_TITLE_RADIUS );
-        ctx.fill();
-
-        ctx.font = "14px Ubuntu";
-        ctx.fillStyle = "#ddd";
-        ctx.fillText( node.name, pX + 16, pY + offsetY * 0.75);
+        
     }
 
     _getVisibleNodes() {
@@ -437,7 +404,7 @@ class GraphCanvas {
 
 }
 
-LX.GraphCanvas = GraphCanvas;
+LX.GraphRenderer = GraphRenderer;
 
 /**
  * @class Graph
@@ -457,7 +424,6 @@ class Graph {
         this.nodes = [
             new GraphNode({
                 name: "Node 1",
-                xsize: [120, 100],
                 position: [200, 200],
                 inputs: [
                     {
@@ -523,7 +489,7 @@ class GraphNode {
         let sX = 16 + this.name.length * 10;
 
         const rows = Math.max(1, Math.max(this.inputs.length, this.outputs.length));
-        let sY = rows * GraphCanvas.NODE_ROW_HEIGHT + GraphCanvas.NODE_TITLE_HEIGHT;
+        let sY = rows * GraphRenderer.NODE_ROW_HEIGHT + GraphRenderer.NODE_TITLE_HEIGHT;
 
         return [sX, sY];
     }
@@ -531,4 +497,4 @@ class GraphNode {
 
 LX.GraphNode = GraphNode;
 
-export { GraphCanvas, Graph, GraphNode };
+export { GraphRenderer, Graph, GraphNode };
