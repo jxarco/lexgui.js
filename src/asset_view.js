100b63536ba713160ccecfa00cb3f9f1bd5bad67
japopra <60257601+japopra@users.noreply.github.com>
2025-09-15 12:30:03 +0200

    fix AssetView undesired scroll

--- src/asset_view.js
+++ src/asset_view.js
@@ -306,7 +306,10 @@ class AssetView {
         }
         else
         {
+            area.root.classList.add("flex");
+            area.root.classList.add("flex-col");
             this.toolsPanel = area.addPanel({ className: 'flex flex-col overflow-hidden', height:"auto" });
+            this.toolsPanel.root.style.flex = "none";
             this.contentPanel = area.addPanel({ className: 'lexassetcontentpanel flex flex-col overflow-hidden' });
         }
 
